06bea618b170ccbfbce07a28eeedc45692db20d5
Kiisi <kiisifelix06@gmail.com>
2023-06-27 18:16:30 +0100

    Updated Hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,10 +4,11 @@ import Image from 'next/image'
 import slate_img from "../public/assets/hero-url-slate-img.svg"
 import anchor_img from '../public/assets/url-anchor.svg'
 import arrow_img from '../public/assets/url-arrow.svg'
+import triangle from '../public/assets/triangle.svg'
 
 const Hero = () => {
   return (
-    <header className="py-[8rem] px-4">
+    <header className="pt-[4rem] pb-[2rem] px-4">
         <div className="w-full max-w-[60rem] mx-auto text-center">
             <h1 className="text-[48px] font-semibold leading-[96px]">Optimize Your Online Experience with Our Advanced <span className="text-primary hero-fancy-text whitespace-nowrap">URL Shortening</span> Solution</h1>
             <p className="text-[18px] leading-[28px] pt-10 max-w-[45rem] mx-auto">Personalize your shortened URLs to align with your brand identity. Utilize custom slugs, branded links, and domain customization options to reinforce your brand presence and enhance user engagement.</p>
@@ -16,8 +17,9 @@ const Hero = () => {
                 <Button className="bg-white !text-primary font-semibold">Learn more</Button>
             </div>
         </div>
-        <div className="pb-14 max-w-fit mx-auto">
-          <div className="rounded-[24px] border-[1px] border-primary min-h-[224px] p-8 w-full max-w-[600px] flex gap-x-5">
+        <div className="pb-14 max-w-fit mx-auto relative"> 
+          <Image src={triangle} alt="Scissor URL Anchor" className="absolute -z-10 top-0 -left-32"/>
+          <div className="rounded-[24px] border-[1px] bg-white border-primary min-h-[224px] p-8 w-full max-w-[600px] flex gap-x-5">
             <figure className="grid place-items-center basis-[40%]">
               <Image src={anchor_img} alt="Scissor URL Anchor"/>
             </figure>
