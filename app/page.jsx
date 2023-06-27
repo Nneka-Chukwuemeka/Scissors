65b831bbdd26c21680581c78521acc18735cd1c0
Kiisi <kiisifelix06@gmail.com>
2023-06-27 17:24:58 +0100

    Feat - Implemented Hero Section in the landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import About from '@/components/About';
+import Hero from '@/components/Hero';
 
 const Home = () => {
 	return (
 		<section className="mt-[3rem] lg:mt-[6rem] z-1 w-full">
+			<Hero/>
 			<About />
 		</section>
 	);
