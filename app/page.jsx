617276f50184a8bc46d8684a2dd78d008999b138
kelvin peter <pkelvin856@gmail.com>
2023-07-15 01:57:56 +0100

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
-
+'use client';
+import React, { useEffect, useState } from 'react';
+import About from '@/components/About';
+import Pricingplan from '@/components/Pricingplan';
+import Hero from '@/components/Hero';
+import '../styles/loader.css';
+import Form from '@/components/Form';
+import Container from '@/components/Container';
+import Navbar from '@/components/Navbar';
+import Getstarted from '@/components/Getstarted';
+import Footer from '@/components/Footer';
+import Faqs from '@/components/Faqs';
+import Sidebar from '@/components/dashboard/Sidebar';
 const Home = () => {
-	return <div>Home</div>;
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(true);
+		}, 3000);
+	}, []);
+	return (
+		<>
+			{loading ? (
+				<section className="">
+					<Navbar />
+					<Hero />
+					<About />
+
+		<Pricingplan />
+		<Container className="w-full flex justify-center items-center p-[1rem] md:p-[2.5rem] h-[25rem] lg:h-[32.6875rem] bg-container">
+			<Form />
+		</Container>
+		<Faqs/>
+		<Container className="w-full flex justify-center items-center p-[1rem] md:p-[2.5rem] h-[25rem] bg-container">
+			<Getstarted />
+		</Container>
+		<Footer/>
+		<Sidebar/>
+	</section>):(<div className='w-screen bg-black  h-screen left-0 top-0 flex flex-col justify-center'>
+        <div className='loader  mx-auto w-10/12'></div>
+       </div>)}
+	
+	</>
+					<Pricingplan />
+					<Container className="w-full flex justify-center items-center p-[1rem] md:p-[2.5rem] h-[25rem] lg:h-[32.6875rem] bg-container">
+						<Form />
+					</Container>
+					<Faqs />
+					<Container className="w-full flex justify-center items-center p-[1rem] md:p-[2.5rem] h-[25rem] bg-container">
+						<Getstarted />
+					</Container>
+					<Footer />
+				</section>
+			) : (
+				<div className="w-screen bg-black  h-screen left-0 top-0 flex flex-col justify-center">
+					<div className="loader  mx-auto w-10/12"></div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Home;
